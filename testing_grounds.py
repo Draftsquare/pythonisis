fea6364cf924a9d3939dd97f43a0476356bdead9
Draftsquare <draftition@gmail.com>
2023-11-08 11:41:54 -0500

    Updated .py files due to experimentation with code, progress on creating a working 'market'/Amazon like mock-up has continued smoothly

--- testing_grounds.py
+++ testing_grounds.py
@@ -1,7 +1,55 @@
-chr = "e"
+categories = [
+    "Candy",
+    "Chips",
+    "Drinks",
+    "Giftcards"
+]
 
-UserResponse = input("press the e button:")
-if UserResponse == chr:
-    print("nice")
-    print("i always run")
+items = [
+    [ 
+        ["Hersheys", 2.99],
+        ["Sourpatch kids", 1.99],
+        ["Jolly ranchers", 2.89 ],
+    ],
+    [ 
+        ["Hersheys", 2.99],
+        ["Sourpatch kids", 1.99],
+        ["Jolly ranchers", 2.89 ],
+    ],
+    [ 
+        ["Hersheys", 2.99],
+        ["Sourpatch kids", 1.99],
+        ["Jolly ranchers", 2.89 ],
+    ],[ 
+        ["Hersheys", 2.99],
+        ["Sourpatch kids", 1.99],
+        ["Jolly ranchers", 2.89 ],
+    ],
 
+    
+]
+
+# ["Lays", "Doritos", "Pop corners" ],
+# ["Gatorade", "Coca Cola", "Pepsi" ],
+# ["Steam", "Amazon", "Gamestop" ]
+
+WantsToShop = True
+
+
+while WantsToShop == True:
+        print("Categories:")
+        for index, category in enumerate(categories):
+            print(f"{category}")
+
+        category_choice = int(input("Select a category (1-4):") or 0) - 1
+        if 0 <= category_choice < len(categories):
+            print(f"Items in the '{categories[category_choice]}' category:")
+            for item in items[category_choice]:
+                print(f"- {item}")
+
+        else:
+                print("Invalid category choice. Please choose a number between 1 and 4.")
+        user_input = input("Continue shopping? (y/n): ")
+        if user_input.lower() == "n":
+              WantsToShop = False
+              print("Exiting the program.")
--- while/test while.py
+++ while/test while.py
@@ -1,13 +1,11 @@
 button_pressed = False
 
 while not button_pressed:
-    user_input = input("Press a button or type exit to exit")
+    user_input = input("type exit to exit:")
     if user_input == "exit":
         break
 
-    if user_input == "desired_button":
-        button_pressed = True
-        print("nice")
+   
     else:
         print("no press, try")
 
